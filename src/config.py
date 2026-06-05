ac099e1dca612596a7c0eb4061a153f0e332fa7c
deshmukhyouraj07-byte <deshmukhyouraj07@gmail.com>
2026-06-05 22:22:14 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,14 @@
- 
+ import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+class Config:
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    LLM = ChatOpenAI(
+        model="gpt-4o-mini", 
+        temperature=0.1, 
+        api_key=OPENAI_API_KEY
+    )
+
